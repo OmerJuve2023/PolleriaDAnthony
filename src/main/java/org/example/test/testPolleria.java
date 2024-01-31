ac21d55f95888b50defffe8c145990b213102046
Omar Rafael Toribio <106570431+OmerJuve2023@users.noreply.github.com>
2024-01-31 14:51:06 -0500

    add README.md

--- src/main/java/org/example/test/testPolleria.java
+++ src/main/java/org/example/test/testPolleria.java
@@ -6,7 +6,6 @@ import org.example.model.precioMezaPolleria;
 import java.util.ArrayList;
 
 public class testPolleria {
-    static precioMezaPolleria[] matriz = new precioMezaPolleria[3];
 
     public static void main(String[] args) {
 
